d53e0f9fe1b1b7e16eea32f9e314a5990297cf2c
Charles Kuo <chao.kuo@gmail.com>
2017-01-30 01:18:05 -0800

    Creating Filtering Extension Methods - p.80

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -100,5 +100,25 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)String.Format("Cart Total: {0}, Array Total: {1}", cartTotal, arrayTotal));
         }
+
+        public ViewResult UseFilterExtensionMethod() {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product> {
+                    new Product { Name = "Kayak", Category = "Watersports", Price = 275M },
+                    new Product { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+                    new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                    new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+                }
+            };
+            string category = "Soccer";
+            decimal total = 0;
+            foreach (Product prod in products.FilterByCategory(category))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Category: {0}; Total: {1}", category, total));
+        }
     }
 }
--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -18,5 +18,14 @@ namespace LanguageFeatures.Models
             }
             return total;
         }
+
+        public static IEnumerable<Product> FilterByCategory(
+                this IEnumerable<Product> productEnum, string categoryParam) {
+            foreach (Product prod in productEnum) {
+                if (prod.Category == categoryParam) {
+                    yield return prod;
+                }
+            }
+        }
     }
 }
